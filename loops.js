25616fb7ddd4debbb449a0844c27473c11fcf25c
Kevin Boston <kbos22@gmail.com>
2020-03-31 13:50:33 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
-let forLoop(arr){
+let fLoop = function forLoop(arr){
   for (let i = 0; i<25; i++){
     if (i === 1){
-      
+      arr.push("I am 1 strange loop.");
     } else {
-      "I am `${i}` strange loops."
+      arr.push("I am `${i}` strange loops.");
     }
   }
   return arr;
